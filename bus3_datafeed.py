8e4072e461e21c40bb369ea1a56176f46816a184
lt47 <lt47@users.noreply.github.com>
2020-02-10 10:18:08 -0600

    auto push

--- bus3_datafeed.py
+++ bus3_datafeed.py
@@ -6,6 +6,8 @@ while True:
     # Filter the data accordingly.
     data = data[data['name'] == 'Bus 3']
     data.to_csv('bus3_datafeed.csv', index=False, header=None, mode='w')
+    lastrow = data.tail(1)
+    lastrow.to_csv('bus3_location.csv', index=False, header=None, mode='w')
     print(data)
     time.sleep(1)
 
