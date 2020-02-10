ea07b7986cf6af2f1dd3a794c3330c52612fd74f
lt47 <lt47@users.noreply.github.com>
2020-02-10 12:18:09 -0600

    auto push

--- testsen_exceptiondatafeed.py
+++ testsen_exceptiondatafeed.py
@@ -1,6 +1,6 @@
 import csv, json
 
-with open('senate_exceptiondatafeed.csv', 'rb') as csvdata:
+with open('senate_exceptiondatafeed.csv', 'rt') as csvdata:
     next(csvdata, None) # skip the headers
     reader = csv.DictReader(csvdata,fieldnames=['datetime', 'name','rule'])
     json.dump([row for row in reader], open('testsen_exceptiondatafeed.json', 'w+'))
